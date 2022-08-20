bac5950f4e1c18c41e76e0786bb364215d9d8d43
HelithaSri <helitha.pravin@gmail.com>
2022-08-20 20:14:16 +0530

    Order CRUD Done

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const customer = require('./routes/customer')
 const item = require('./routes/Item')
+const order = require('./routes/Order')
 const app = express()
 const port = 5000
 
 app.use(express.json());
 
+app.use('/customer',customer)
 app.use('/item',item)
+app.use('/order',order)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
