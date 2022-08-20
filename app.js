165d466479f1bb3b6e8f3a3be4f57b3fb670e37e
HelithaSri <helitha.pravin@gmail.com>
2022-08-20 13:45:19 +0530

    add new routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const customer = require('./routes/customer')
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.use('/customer',customer)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
